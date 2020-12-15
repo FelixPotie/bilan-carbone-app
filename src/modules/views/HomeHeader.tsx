757604feae8ff2c32470ce392b55bf9639740bfc
Simon <gayetsimon@gmail.com>
2020-12-15 22:57:58 +0100

    Homepage header, navbar color font

---
+++ src/modules/views/HomeHeader.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  withStyles,
+  Theme,
+  WithStyles,
+  createStyles,
+} from '@material-ui/core/styles';
+import Button from '../components/Button';
+import Typography from '../components/Typography';
+import HomeHeaderLayout from './HomeHeaderLayout';
+import { useTranslation} from 'react-i18next'
+
+const styles = (theme: Theme) =>
+  createStyles({
+    button: {
+      minWidth: 200,
+      color: 'white',
+      fontWeight: 900,//theme.typography.fontWeightRegular,
+      fontSize: 20
+    },
+    
+    h5: {
+      marginBottom: theme.spacing(4),
+      marginTop: theme.spacing(4),
+      [theme.breakpoints.up('sm')]: {
+        marginTop: theme.spacing(10),
+      },
+    },
+    more: {
+      marginTop: theme.spacing(2),
+    },
+  });
+
+function HomeHeader(props: WithStyles<typeof styles>) {
+  const { classes } = props;
+
+  const  {t, i18n} = useTranslation('homePage');
+
+  return (
+    <HomeHeaderLayout >
+      {/* Increase the network loading priority of the background image. */}
+      <Typography color="inherit" align="center" variant="h2" marked="center">
+       {t("SIMULATE_YOUR_TRAJECT")}
+      </Typography>
+      <Typography color="inherit" align="center" variant="h5" className={classes.h5}>
+       {t("ENTER_SIMULATE_DESCIPTION")}
+      </Typography>
+      <Button
+        color="secondary"
+        variant="contained"
+        size="large"
+        className={classes.button}
+        component="a"
+        href="/premium-themes/onepirate/sign-up/"
+      >
+        {t("LAUNCH_SIMULATION")}
+      </Button>
+    </HomeHeaderLayout>
+  );
+}
+
+export default withStyles(styles)(HomeHeader);
