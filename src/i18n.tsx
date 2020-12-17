a50853cb36f8f233ce8abf6dbe933603a10e19c7
Simon <gayetsimon@gmail.com>
2020-12-17 14:12:47 +0100

    lang selection in footer

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,12 +1,13 @@
 import i18n from 'i18next'
 import Backend from 'i18next-xhr-backend'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
   .use(Backend)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    lng: 'en',
     backend: {
       /* translation file path */
       loadPath: '/assets/i18n/{{ns}}/{{lng}}.json'
@@ -14,7 +15,7 @@ i18n
     fallbackLng: 'en',
     debug: true,
     /* can have multiple namespace, in case you want to divide a huge translation into smaller pieces and load them on demand */
-    ns: ['translations'],
+    ns: ['translations','homePage'],
     defaultNS: 'translations',
     interpolation: {
       escapeValue: false,
@@ -22,7 +23,8 @@ i18n
     },
     react: {
       wait: true,
-      useSuspense: false
+      useSuspense: false,
+      bindI18n: 'languageChanged loaded',
     }
   })
 
