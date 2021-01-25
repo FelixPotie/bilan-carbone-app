a491263cacfad1fa8f4e32c25af3a17716a69345
Simon <gayetsimon@gmail.com>
2021-01-25 17:43:01 +0100

    roll back to old language select

--- src/locales/i18n.tsx
+++ src/locales/i18n.tsx
@@ -1,16 +1,15 @@
 import i18n from 'i18next'
 import Backend from 'i18next-xhr-backend'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector';
+
 
 i18n
   .use(Backend)
+    .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    lng:'fr-FR',
-    fallbackLng:(code) => {
-      if (!code || code === 'en') return ['en-US'];
-      const fallbacks = [code];
-      return fallbacks},
+    fallbackLng:'fr-FR',
     backend: {
       /* translation file path */
       loadPath: '/assets/i18n/{{ns}}/{{lng}}.json'
