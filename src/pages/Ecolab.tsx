6729b06c753f564b13a5e99346af003fe84cb8e8
felixpotie <felixpotie@gmail.com>
2021-04-01 22:13:54 +0200

    fix ecolab page

--- src/pages/Ecolab.tsx
+++ src/pages/Ecolab.tsx
@@ -3,10 +3,24 @@ import React, { useEffect, useState } from 'react'
 import Auth from '../modules/components/Auth';
 import Typography from '../modules/components/Typography';
 import withRoot from '../modules/withRoot';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    box:{
+        margin: 'auto',
+        width: '50%',
+    },
+    title:{
+        marginTop: theme.spacing(2),
+        marginBottom: theme.spacing(2),
+        marginRight: '5%',
+        marginLeft:  '5%',
+    }
+}));
 
 function Ecolab() {
     const [loaded, setLoaded] = useState(false)
-
+    const classes = useStyles();
 
     useEffect(() => {
         if (document.getElementById('test') === null) {
@@ -14,9 +28,6 @@ function Ecolab() {
             scriptTag.src = 'https://ecolab.ademe.fr/apps/transport/iframe.js'
             scriptTag.id = 'test'
             scriptTag.async = true
-            // scriptTag.style.display = "block"
-            scriptTag.style.width = "300px"
-            scriptTag.style.height = "300px"
 
             scriptTag.addEventListener('load', () => setLoaded(true))
             document.body.appendChild(scriptTag)
@@ -27,17 +38,20 @@ function Ecolab() {
         if (!loaded) return;
     }, [loaded])
 
-
     return (
         <React.Fragment>
             <Auth/>
             <Container>
-                <Box>
-                    <Typography variant="h3" gutterBottom marked="center" align="center">
-                        Simulez votre trajet !
-                    </Typography>
-                    <div style={{"height":"1100px", "margin":"20px"}}>
-                        <script  style={{"height":"1070px"}} id="ecolab-transport" data-distanceInitiale="29" src="https://ecolab.ademe.fr/apps/transport/iframe.js"></script>
+                <Box display="flex" className={classes.title}>
+                    <Box m="auto">
+                        <Typography variant="h3" gutterBottom marked="center" align="left">
+                            Simulez votre trajet !
+                        </Typography>
+                    </Box>
+                </Box>
+                <Box className={classes.box}>
+                    <div>
+                        <script  style={{"height":"100%"}} id="ecolab-transport" data-distanceInitiale="29" src="https://ecolab.ademe.fr/apps/transport/iframe.js"></script>
                     </div>
                 </Box>
             </Container>
